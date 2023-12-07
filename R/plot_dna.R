fe97347d83cf49c642a5028514ea18184c38ae59
Bruno Mioto <brunomioto97@gmail.com>
2023-12-06 22:30:26 -0300

    Initial commit

---
+++ R/plot_dna.R
@@ -0,0 +1,70 @@
+#' Plot DNA sequence
+#'
+#' Creates a plot of a DNA sequence, with each base represented by a colored rectangle.
+#'
+#' @param seq A character string representing the DNA sequence.
+#'
+#' @return A `ggplot2` object representing the DNA sequence plot.
+#'
+#' @examples
+#'
+#' seq <- "ATCGATCG"
+#' plot_dna(seq)
+#'
+#' @importFrom ggplot2 ggplot aes geom_text geom_rect annotate scale_fill_manual theme_void theme element_rect coord_equal
+#'
+#' @export
+#'
+plot_dna <- function(seq) {
+
+  vec <- strsplit(toupper(seq), "")[[1]]
+
+  length(vec)
+
+  plot <- data.frame(
+    x = seq(0,length(vec)-1,1),
+    y = 0,
+    b = vec
+    ) |>
+    ggplot(aes(x = x,
+               y = y))+
+    geom_text(aes(label = b,
+                  y = y+0.1))+
+    geom_rect(
+      aes(xmin = x-0.1,
+          xmax = x+0.1,
+          ymin = 0,
+          ymax = -0.5,
+          fill = b)
+    )+
+    annotate(
+      "rect",
+      xmin = -0.3,
+      xmax = length(vec)-1+0.3,
+      ymin = 0,
+      ymax = -0.2
+    )+
+    scale_fill_manual(
+      values = c(
+        "A" = "#e41a1c",
+        "C" = "#377eb8",
+        "T" = "#4daf4a",
+        "G" = "#984ea3"
+      )
+    )+
+    theme_void()+
+    theme(
+      plot.background = element_rect(fill = "white",
+                                     color = NA),
+      legend.position = "none"
+    )+
+    coord_equal(
+      clip = "off"
+    )+
+    NULL
+
+  cli::cli_alert_info("Plotting {length(vec)} bases")
+
+  return(plot)
+
+}
